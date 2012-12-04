c69a0c267d42d4a080a2cc0f9109ec56aeae600d
康悦 <kangyue@kangyuetekiMacBook-Pro.local>
2012-12-04 20:19:57 +0800

    Initial Commit

---
+++ market-1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  market-1
+//
+//  Created by 康悦 on 12-12-4.
+//  Copyright (c) 2012年 ky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
